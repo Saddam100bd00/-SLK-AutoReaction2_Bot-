ab7d255e61ec31fbb584d9521392ce2adc68ca60
Saddam100bd00 <saddampersonallife@gmail.com>
2026-09-15 23:18:03 +0600

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,14 +1,17 @@
 from flask import Flask
 from threading import Thread
+import os
 
-app = Flask(__app__)
+app = Flask(__name__) # <--- এই লাইনটাই আসল সমস্যা ছিল
 
 @app.route('/')
 def home():
-    return "Bot is Alive and Running!"
+    return "Auto Reaction Bot is Alive and Running!"
 
 def run():
-    app.run(host='0.0.0.0', port=8080)
+    # Render থেকে আসা নির্দিষ্ট পোর্ট অথবা ডিফল্ট 8080 ব্যবহার করবে
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host='0.0.0.0', port=port)
 
 def keep_alive():
     t = Thread(target=run)
